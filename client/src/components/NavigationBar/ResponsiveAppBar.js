c12c1a80c65c2ad5f8b1baa5dbe3e506ded9b149
zwang2244 <zwang2244@wisc.edu>
2021-12-03 19:56:05 -0600

    nav bar done

--- client/src/components/NavigationBar/ResponsiveAppBar.js
+++ client/src/components/NavigationBar/ResponsiveAppBar.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import {AppBar, Box, Toolbar, IconButton, Typography, Menu, Container, Avatar, Button, Tooltip, MenuItem} from '@material-ui/core';
-import {createMuiTheme, MuiThemeProvider, makeStyles} from '@material-ui/core/styles';
-// import Unlogginprofile from '../images/Unlogginprofile';
+import "./ResponsiveAppBar.css";
 const pages = ['About', 'Home'];
 const settings = ['My Home', 'Log In', 'Sign Out'];
 
@@ -26,65 +25,61 @@ const ResponsiveAppBar = () => {
     };
   
     return (
-      <div style={{ width: '100%' }}>
+      <div>
       <AppBar position="fixed">
-        <Container
-          >
-          <Toolbar disableGutters>
-            <Typography
-              variant="h5"
-            >
-              PetitionRate
-            </Typography>
+        <div className = "nav_bar">
 
-            <Box sx={{
-            display: 'inline-flex',
-            flexDirection: 'row',
-            justifyContent: 'space-between' 
-            }} m={2} >
-            
-              {pages.map((page) => (
-                <Button
-                  key={page}
-                  onClick={handleCloseNavMenu}
-                  variant="contained"
-                  color = "primary"
-                >
-                  {page}
-                </Button>
-              ))}
-            
-              <Tooltip title="Open Profile" >
-                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}  >
-                  {/* <Avatar alt="Remy Sharp" src="../images/Unlogginprofile.png" /> */}
-                  <Avatar src="/broken-image.jpg" />
-                </IconButton>
-              </Tooltip>
-              <Menu
-                sx={{ mt: '45px' }}
-                id="menu-appbar"
-                anchorEl={anchorElUser}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={Boolean(anchorElUser)}
-                onClose={handleCloseUserMenu}
+          <div className = "nav_left">
+            <div className = "nav_title">
+              <Typography
+                variant="h5"
               >
-                {settings.map((setting) => (
-                  <MenuItem key={setting} onClick={handleCloseNavMenu}>
-                    <Typography textAlign="center">{setting}</Typography>
-                  </MenuItem>
-                ))}
-              </Menu>
-            </Box>
-          </Toolbar>
-        </Container>
+                PetitionRate
+              </Typography>
+            </div>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                variant="contained"
+                color = "primary"
+                className = "nav_btn"
+              >
+                {page}
+              </Button>
+            ))}
+          </div>
+
+          <Tooltip title="Open Profile" >
+            <div className = "nav_profile">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}  >
+                <Avatar src="/broken-image.jpg" />
+              </IconButton>
+            </div>
+          </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+          </Menu>
+        </div>
       </AppBar>
       </div>
     );
